05913a58e4f5312aa48f0ac1bb3e7dc9a7856153
sabdi21 <sabdi21@hotmail.com>
2019-07-31 14:50:33 -0700

    started project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # quiz-group-project
+
+question 1: Which is not a "Game of Thrones" royal family name?
+answers = Stark, Tyrell, Targaryen, Baelish
+
---
+++ app.js
@@ -0,0 +1,27 @@
+//Global Variables
+var reset;
+var score = 0;
+var buttonClick = $('#start-button').click(function(){
+    console.log("button was clicked")
+    resetQuiz();
+})
+
+// keep track of current question
+
+// update users score
+function updateScore(){
+
+};
+
+// check for timeout
+function setTimer(){
+
+};
+
+// START QUIZ LOAD
+function resetQuiz(){
+    $('#start-button').visibility = 'hidden';  
+
+    
+};
+
