dbece6de310238d5d1af548fcd10699165f1c88e
sabdi21 <sabdi21@hotmail.com>
2019-07-31 16:28:52 -0700

    most events working

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var resultsPane = $('#resuls').hide();
 var previousBtn = $('#previous-btn').hide();
 var nextBtn = $('#next-btn').hide();
 var currentQuestion = 0;
-
+var questionAnswered = false;
 var reset;
 var score = 0;
 
@@ -15,7 +15,8 @@ var score = 0;
 let questions = [{
     prompt: "Which is not a Game of Thrones royal family name?",
     answers: ["Stark", "Tyrell", "Targaryen", "Baelish"],
-    correctAnswerIndex: 3
+    correctAnswerIndex: 3, 
+    userAnswer: -1
   }];
 
 const NUM_QUESTIONS = questions.length;
@@ -25,8 +26,6 @@ var buttonClick = $('#start-btn').click(function(){
     console.log("button was clicked")
     resetQuiz();
 })
-
-// keep track of current question
 
 // update users score
 function updateScore(){
@@ -51,12 +50,31 @@ function resetQuiz(){
     displayQuestion();    
 };
 
+// keep track of current question
 function displayQuestion() {
+    questionAnswered= false;
+
     questionsPane.append("<p>" + questions[currentQuestion].prompt + "</p>")
     console.log(answers);
     for(let i = 0; i < questions[currentQuestion].answers.length; i++) {
-        answersPane.append(`<input type="radio" name="questions" value="` + questions[currentQuestion].answers[i] + `">`);
-        // console.log("loops iteration", i);
-        // answersPane.append("<p>Test</p>")
+        answersPane.append(`<input type="radio" name="questions" value="` + i + `">` + questions[currentQuestion].answers[i]);
     }
+
+    answersPane.click(function() {
+        questions[currentQuestion].userAnswer = event.target.value;
+        questionAnswered = true;
+    })
+
+    nextBtn.click(handleNextClick);
 }
+
+function handleNextClick() {
+    if (currentQuestion === questions.length) {
+       displayResults(); 
+    }
+    else {
+        currentQuestion++;
+        displayQuestion();
+    }
+       
+};
