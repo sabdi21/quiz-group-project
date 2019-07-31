f809720df582c260d7093f748c2e15cecaa0ec6c
sabdi21 <sabdi21@hotmail.com>
2019-07-31 16:07:28 -0700

    event selectors done-ish

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 question 1: Which is not a "Game of Thrones" royal family name?
 answers = Stark, Tyrell, Targaryen, Baelish
 
+let questions = [{
+  prompt: "Which is not a "Game of Thrones" royal family name?",
+  answers: ["Stark", "Tyrell", "Targaryen", "Baelish"],
+  correctAnswerIndex: 0
+}];
--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 
-// Global Variables
-
 //Global Variables
+var questionsPane = $('#question').hide();
+var answersPane = $('#answers').hide();
+// console.log(answersPane);
+var resultsPane = $('#resuls').hide();
+var previousBtn = $('#previous-btn').hide();
+var nextBtn = $('#next-btn').hide();
+var currentQuestion = 0;
+
 var reset;
 var score = 0;
-var buttonClick = $('#start-button').click(function(){
+
+// QUESTIONS
+let questions = [{
+    prompt: "Which is not a Game of Thrones royal family name?",
+    answers: ["Stark", "Tyrell", "Targaryen", "Baelish"],
+    correctAnswerIndex: 3
+  }];
+
+const NUM_QUESTIONS = questions.length;
+
+
+var buttonClick = $('#start-btn').click(function(){
     console.log("button was clicked")
     resetQuiz();
 })
@@ -23,7 +40,23 @@ function setTimer(){
 
 // START QUIZ LOAD
 function resetQuiz(){
-    $("#start-button").hide();
+    // $("#start-btn").hide();
+    questionsPane.show();
+    answersPane.show();
+    previousBtn.show();
+    nextBtn.show();
+    buttonClick.prop('value', 'Reset');
+    currentQuestion = 0;
 
-    
+    displayQuestion();    
 };
+
+function displayQuestion() {
+    questionsPane.append("<p>" + questions[currentQuestion].prompt + "</p>")
+    console.log(answers);
+    for(let i = 0; i < questions[currentQuestion].answers.length; i++) {
+        answersPane.append(`<input type="radio" name="questions" value="` + questions[currentQuestion].answers[i] + `">`);
+        // console.log("loops iteration", i);
+        // answersPane.append("<p>Test</p>")
+    }
+}
