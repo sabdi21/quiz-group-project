83ebbd33a67a032c3998f8a9145576bb366ad92f
gtole342 <gtole342@gmail.com>
2019-07-31 19:09:24 -0700

    Fixed few more bugs

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var answersPane = $('#answers').hide();
 var resultsPane = $('#results').hide();
 var previousBtn = $('#previous-btn').hide();
 var nextBtn = $('#next-btn').hide();
+var startBtn = $('#start-btn');
 var currentQuestion = 0;
 var questionAnswered = false;
 var reset;
 var score = 0;
 
 // QUESTIONS
-let questions = [{
+var questions = [{
     prompt: "Which is not a Game of Thrones royal family name?",
     answers: ["Stark", "Tyrell", "Targaryen", "Baelish"],
     correctAnswerIndex: 3, 
@@ -42,30 +43,60 @@ let questions = [{
 const NUM_QUESTIONS = questions.length;
 
 
-var buttonClick = $('#start-btn').click(function(){
+var buttonClick = startBtn.click(function(){
     console.log("button was clicked")
     resetQuiz();
 })
 
-// update users score
-function updateScore(){
+// // update users score
+// function updateScore(){
 
-};
+// };
 
-// check for timeout
-function setTimer(){
+// // check for timeout
+// function setTimer(){
 
-};
+// };
+function resetUserAnswers(){
+    questions = [{
+        prompt: "Which is not a Game of Thrones royal family name?",
+        answers: ["Stark", "Tyrell", "Targaryen", "Baelish"],
+        correctAnswerIndex: 3, 
+        userAnswer: -1
+    }, {
+        prompt: "Ayra's fighting style is called?",
+        answers: ["Wolf Wield", "Water Dancing", "Stinger", "Slashing Steel"],
+        correctAnswerIndex: 1, 
+        userAnswer: -1
+    }, {
+        prompt: "Sandor Clegane is known as ...",
+        answers: ["The Beast", "The Kingslayer", "The Mountain", "The Hound"],
+        correctAnswerIndex: 3, 
+        userAnswer: -1
+    }, {
+        prompt: "Bran Stark is paralyzed following a fall, who pushed him?",
+        answers: ["Tryon Lannister", "Jaime Lannister", "Cersei Lannister", "Joffrey Baratheon"],
+        correctAnswerIndex: 1, 
+        userAnswer: -1
+    }, {
+        prompt: "According to Bran, \"chaos is ...\"",
+        answers: ["a staircase", "a ladder", "Inevitable", "a way of life"],
+        correctAnswerIndex: 1, 
+        userAnswer: -1
+      }];
+}   
 
 // START QUIZ LOAD
 function resetQuiz(){
     // $("#start-btn").hide();
+    resultsPane.empty();
     questionsPane.show();
     answersPane.show();
     previousBtn.show();
     nextBtn.show();
     buttonClick.prop('value', 'Reset');
     currentQuestion = 0;
+
     previousBtn.click(handlePrevClick);
     nextBtn.click(handleNextClick);
     displayQuestion();    
@@ -93,6 +124,7 @@ function displayQuestion() {
 }
 function handlePrevClick(){
     questions[currentQuestion].userAnswer = -1
+    resultsPane.empty()
     currentQuestion--
     displayQuestion()
 }
